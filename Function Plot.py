3ae0cd330a945fb340e105a944a327ca418d39dd
Rahul Choudhary <rahul0101rock@gmail.com>
2020-09-22 23:30:40 +0530

    Update Function Plot.py

--- Function Plot.py
+++ Function Plot.py
@@ -1,18 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-inp=input("Enter the Function: ")
+inp=input("Enter the Function (In y=mx+c format)\n-> ")
 if '+' not in inp:
-    b=0
+    c=0
 else:
-    b=int(inp[inp.index('+')+1:])
+    c=int(inp[inp.index('+')+1:])
 if '=x' not in inp:
     m=int(inp[inp.index('=')+1:inp.index('x')])
 else:
     m=1
-x = [-(2*b),2*b]
+if c==0:
+    x=[-10,10]
+else:
+    x = [-(2*c),2*c]
 plt.figure(figsize=(8,8)) 
-y = m*np.array(x)+b
+y = c*np.array(x)+c
 plt.plot(x,y,label=inp)
 plt.xlim(x) 
 plt.ylim(x) 
