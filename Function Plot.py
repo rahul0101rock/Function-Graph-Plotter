24083357a4bbeff099ec3844c58290b25eba0fe5
Rahul Choudhary <rahul0101rock@gmail.com>
2020-09-23 15:04:36 +0530

    Optimized Constant Finding and point plot

--- Function Plot.py
+++ Function Plot.py
@@ -6,11 +6,15 @@ from PIL import Image
 inp=input("Enter the Function\n-> ")
 inp=inp.lower()
 c=1
-point=0
+ix,iy=0,0
 if inp.find('^x')>0:c=20
 if inp[-1]!='x':
-    c=int(re.findall(r'\D\d+',inp)[0][1:])
-    point=c
+    last=re.findall(r'\D\d+$',inp)[0]
+    c=int(last[1:])
+    if '-' in last:
+        ix,iy=c,0
+    elif '+' in last:
+        ix,iy=0,c
     c=2*c
 x=np.linspace(-c,c,10000)
 plt.figure(figsize=(8,8)) 
@@ -23,13 +27,15 @@ for s in re.findall(r'\D\d+\D',inp):
 expinp=expinp.replace('x','np.array(x)',expinp.count("x"))
 exec(expinp)
 plt.plot(x,y,label=inp)
-# plt.plot(0,point,'ro')
-# plt.annotate('  (0,{})'.format(point), (0, point))
+
 if '^' in inp:
-    plt.xlim([-max(x)*2,max(x)*2]) 
+    plt.xlim([-max(x)*2,max(x)*2])
+    ix,iy=0,int(last)
 else:
     plt.xlim([-max(x),max(x)]) 
 plt.ylim([-max(y),max(y)]) 
+plt.plot(ix,iy,'ro')
+plt.annotate('  ({},{})'.format(ix,iy), (ix, iy))
 axis = plt.gca()
 plt.plot(axis.get_xlim(),[0,0],'--')
 plt.plot([0,0],axis.get_ylim(),'--')
